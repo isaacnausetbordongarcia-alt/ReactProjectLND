48b6d286f40d124f0eb6e9347022826b6eb5d708
isaac <isaacnausetbordongarcia@alumno.ieselrincon.es>
2026-02-13 01:18:24 +0000

    fixed circuit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
-import Schedule from "./pages/schedule/schedule"
+import Schedule from "./pages/schedule/Schedule"
 
 function App() {
   return (
--- src/components/circuit/Circuit.jsx
+++ src/components/circuit/Circuit.jsx
@@ -1,11 +1,30 @@
-function Circuit(props) {
+import "./Circuit.css";
 
-    return (
-        <div className="circuit-info">
-            <div className="circuits-flags"><img src={props.circuitFlag} alt="" /></div>
-            <div className="circuits-grand-prix-map"><img src={props.circuitMap} alt=""/></div>
+function Circuit(props) {
+  return (
+    <div className="circuit-card-container">
+      <div className="circuit-card">
+        <div className="circuit-text">
+          <p className="circuit-country">{props.circuitCountry}</p>
+
+          <p className="circuit-gp">
+            FORMULA 1 HEINEKEN CHINESE GRAND PRIX 2026
+          </p>
+          <p className="circuit-date">{props.circuitDate}</p>
         </div>
-    )
+
+        <div className="circuit-right">
+          <div className="circuits-flags-container">
+            <img className="circuits-imgs" src={props.circuitFlag} alt="" />
+          </div>
+          <div className="circuits-grand-prix-map-container">
+            <img className="circuits-grand-prix-map" src={props.circuitMap} alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Circuit
+export default Circuit;
